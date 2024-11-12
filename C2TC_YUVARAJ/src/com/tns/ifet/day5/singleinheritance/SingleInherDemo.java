90d8a546d32750f83c0803b628a1d2c60a01ada1
Yuvaraj <yuvaraj33600@gmail.com>
2024-11-12 21:27:46 +0530

    Commit

---
+++ C2TC_YUVARAJ/src/com/tns/ifet/day5/singleinheritance/SingleInherDemo.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day5.singleinheritance;
+public class SingleInherDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+
+}

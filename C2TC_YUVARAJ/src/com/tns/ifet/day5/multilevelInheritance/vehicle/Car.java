75f31143f242b2dbf5353af1a67a4115fefe6530
Yuvaraj <yuvaraj33600@gmail.com>
2024-11-13 07:41:21 +0530

    commit

---
+++ C2TC_YUVARAJ/src/com/tns/ifet/day5/multilevelInheritance/vehicle/Car.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day5.multilevelInheritance.vehicle;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+}
+	
